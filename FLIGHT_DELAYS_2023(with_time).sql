1a9d6c52f140346a726c1019629c84fdcd093441
Maria Papadopoulou <143605460+PapadopoulouMaria@users.noreply.github.com>
2024-01-03 23:43:39 +0200

    Add files via upload

---
+++ FLIGHT_DELAYS_2023(with_time).sql
@@ -0,0 +1,23 @@
+-- FL_DATE:	Flight Date (yyyymmdd) --
+-- ARR_DELAY: Difference in minutes between scheduled and actual arrival time. Early arrivals show negative numbers --
+-- FL_NUMBER: Flight Number --
+-- ORIGIN_CITY:	Origin Airport, City Name --
+-- CANCELLED: Cancelled Flight Indicator (1=Yes) --
+-- DELAY_DUE_CARRIER: Carrier Delay, in Minutes --
+-- DELAY_DUE_WEATHER: Weather Delay, in Minutes --
+-- DELAY_DUE_NAS: National Air System Delay, in Minutes --
+-- DELAY_DUE_SECURITY: Security Delay, in Minutes --
+-- DELAY_DUE_LATE_AIRCRAFT:	Late Aircraft Delay, in Minutes --
+
+SELECT AIRLINE,
+FL_DATE,
+TIME_FORMAT(sec_to_time(ARR_DELAY*60), '%H:%i') AS 'ARR_DELAY',
+FL_NUMBER,
+ORIGIN_CITY,
+CANCELLED,
+TIME_FORMAT(sec_to_time(DELAY_DUE_CARRIER*60), '%H:%i') AS 'DELAY_DUE_CARRIER',
+TIME_FORMAT(sec_to_time(DELAY_DUE_WEATHER*60), '%H:%i') AS 'DELAY_DUE_WEATHER',
+TIME_FORMAT(sec_to_time(DELAY_DUE_NAS*60), '%H:%i') AS 'DELAY_DUE_NAS',
+TIME_FORMAT(sec_to_time(DELAY_DUE_SECURITY*60), '%H:%i') AS 'DELAY_DUE_SECURITY',
+TIME_FORMAT(sec_to_time(DELAY_DUE_LATE_AIRCRAFT*60), '%H:%i') AS 'DELAY_DUE_AIRCRAFT'
+FROM airplane_delays_2023.flight_delay_2023;
